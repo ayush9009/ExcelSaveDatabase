a5d2f9fa55dc2d7535e2bc46e66f811e748dd231
Ayush Sharma <97290036+ayush9009@users.noreply.github.com>
2024-04-22 23:51:03 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -5,7 +5,7 @@ function connectDB() {
     mongoose.connect("mongodb://localhost/neemtreeassignment", { useNewUrlParser: true });
     const db = mongoose.connection
     db.on('error', (error) => console.log(error));
-    db.once('open', () => console.log('MongoDB Connected !!!'));
+    db.once('open', () => console.log('MongoDB Connected !!'));
 }
 
 module.exports = connectDB;
